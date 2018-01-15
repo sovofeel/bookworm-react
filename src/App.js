77e37edced402fd2b7999abd32818bfb6dd17bbf
sovofeel <sovofeel@gmail.com>
2018-01-15 07:18:24 +0500

    create login and home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
-const App = () => <div>Hello world</div>;
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+      </div>
+    );
+  }
+}
 
 export default App;
